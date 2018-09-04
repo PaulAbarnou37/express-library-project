9a9fac9c1db4daf2736ef4fad7d161625282f1c4
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-09-04 10:33:01 +0200

    Add book create

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,11 @@ router.get("/books", (req, res, next) => {
     .catch(err => next(err));
 });
 
+// This route must be ABOVE "/book/:bookId" (order matters with similar URLs)
+router.get("/book/add", (req, res, next) => {
+  res.render("book-form.hbs");
+});
+
 // Link to this page: /book/999
 router.get("/book/:bookId", (req, res, next) => {
   // get the ID from the URL (it's inside of "req.params")
@@ -33,6 +38,21 @@ router.get("/book/:bookId", (req, res, next) => {
       res.render("book-details.hbs");
     })
     // "next()" means show the error page
+    .catch(err => next(err));
+});
+
+router.post("/process-book", (req, res, next) => {
+  // make variables from the inputs inside "req.body"
+  // (we use "req.body" because it's a POST form submission)
+  const { title, author, description, rating } = req.body;
+
+  // save input variables in our new book
+  Book.create({ title, author, description, rating })
+    .then(bookDoc => {
+      // redirect if it's successful to avoid duplicating the submission
+      // (redirect ONLY to URLs - "/books" instead of "book-list.hbs")
+      res.redirect("/books");
+    })
     .catch(err => next(err));
 });
 
